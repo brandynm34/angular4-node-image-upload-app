65d0ab43135fad31a8884af5a7ea83e8c8869714
brandynm34 <brandynm34@yahoo.com>
2018-03-21 12:05:24 -0700

    part 2

--- users-api/server.js
+++ users-api/server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+const routes = require('./routes/users');
 
-app.listen(port, () => {
-  console.log('Example app listening on port 3000!');
-});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+app.use('/users', routes);
+ 
+app.listen(port);
+console.log('Listening on: ' + port);
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// });
+
+// app.listen(port, () => {
+//   console.log('Example app listening on port 3000!');
+// });
